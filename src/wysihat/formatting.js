446fd95b6905f86fb6d7d19e210108ba0c48d41c
aarongustafson <aaron@easy-designs.net>
2011-09-20 18:18:48 -0400

    Stripping out the convoluted formatting stuff for now…thinking of overhauling it completely.

--- src/wysihat/formatting.js
+++ src/wysihat/formatting.js
@@ -35,8 +35,7 @@ WysiHat.Formatting = (function($){
 			function convertDivsToParagraphs()
 			{
 				$container.find( 'div' ).each(function(){
-					var $this = $(this);
-					$this.replaceWith('<p>' + $this.html() + '</p>');
+					replaceElement( $(this), 'p' );
 				});
 			}
 
@@ -49,6 +48,12 @@ WysiHat.Formatting = (function($){
 			{
 				convertDivsToParagraphs();
 			}
+			
+			// make sure we always start with a paragraph
+			if ( $container.html() == '' )
+			{
+				$container.html('<p><br/></p>');
+			}
 
 			return $container.html();
 
@@ -59,259 +64,256 @@ WysiHat.Formatting = (function($){
 			var
 			$clone	= $el.clone(),
 			el_id	= $el.attr('id'),
-			$p		= $('<p></p>'),
-			mode	= ACCUMULATING_LINE,
-			$result, $container,
-			line, lineContainer,
-			previousAccumulation;
-
-			function walk( nodes )
-			{
-				var
-				length	= nodes.length,
-				node, $node, $newNode, tagName, i;
-
-				for ( i=0; i < length; i++ )
-				{
-					node	= nodes[i];
-					$node	= $(node);
-					
-					//if ( node.nodeType == Node.TEXT_NODE &&
-					//	 $node.parent().attr('id') == el_id )
-					//{
-					//	$newNode = $p.clone().text(node.nodeValue);
-					//	$node.replaceWith( $newNode );
-					//	node = $newNode.get(0);
-					//}
-
-					if ( node.nodeType == Node.ELEMENT_NODE )
-					{
-						tagName = node.tagName.toLowerCase();
-						open( tagName, node );
-						walk( node.childNodes );
-						close( tagName );
-
-					}
-					else if ( node.nodeType == Node.TEXT_NODE )
-					{
-						read( node.nodeValue );
-					}
-				}
-			}
-
-			function open( tagName, node )
-			{
-				$node = $(node);
-
-				if ( mode == ACCUMULATING_LINE )
-				{
-					if ( isBlockElement(tagName) )
-					{
-						if ( isEmptyParagraph( $node ) )
-						{
-							accumulate( $('<br />').get(0) );
-							flush();
-						}
-
-						if ( isListElement(tagName) )
-						{
-							$container	= insertList( tagName );
-							mode		= EXPECTING_LIST_ITEM;
-						}
-					}
-					else if ( isLineBreak(tagName) )
-					{
-						if ( isLineBreak( getPreviouslyAccumulatedTagName() ) )
-						{
-							$(previousAccumulation).remove();
-							flush();
-						}
-
-						accumulate( $node.clone().get(0) );
-
-						if ( ! previousAccumulation.previousNode )
-						{
-							flush();
-						}
-					}
-					else
-					{
-						accumulateInlineElement( tagName, $node );
-					}
-
-				}
-				else if ( mode == EXPECTING_LIST_ITEM )
-				{
-					if ( isListItemElement(tagName) )
-					{
-						mode = ACCUMULATING_LIST_ITEM;
-					}
-
-				}
-				else if ( mode == ACCUMULATING_LIST_ITEM )
-				{
-					if ( isLineBreak(tagName) )
-					{
-						accumulate( $node.clone().get(0) );
-					}
-					else if ( ! isBlockElement(tagName) )
-					{
-						accumulateInlineElement( tagName, $node );
-					}
-				}
-			}
-
-			function close( tagName )
-			{
-				if ( mode == ACCUMULATING_LINE )
-				{
-					if ( isLineElement( tagName ) )
-					{
-						flush();
-					}
-
-					if ( line != lineContainer )
-					{
-						lineContainer = lineContainer.parentNode;
-					}
-				}
-				else if ( mode == EXPECTING_LIST_ITEM )
-				{
-					if ( isListElement(tagName) )
-					{
-						$container = $result;
-						mode = ACCUMULATING_LINE;
-					}
-				}
-				else if ( mode == ACCUMULATING_LIST_ITEM )
-				{
-					if ( isListItemElement(tagName) )
-					{
-						flush();
-						mode = EXPECTING_LIST_ITEM;
-					}
-
-					if ( line != lineContainer )
-					{
-						lineContainer = lineContainer.parentNode;
-					}
-				}
-			}
-
-			function isBlockElement( tagName )
-			{
-				return isLineElement(tagName) || isListElement(tagName);
-			}
-
-			function isLineElement( tagName )
-			{
-				return tagName == "p" || tagName == "div";
-			}
-
-			function isListElement( tagName )
-			{
-				return tagName == "ol" || tagName == "ul";
-			}
-
-			function isListItemElement(tagName)
-			{
-				return tagName == "li";
-			}
-
-			function isLineBreak(tagName)
-			{
-				return tagName == "br";
-			}
-
-			function isEmptyParagraph( $node )
-			{
-				return $node.is('p') && $node.html() == '';
-			}
-
-			function read(value)
-			{
-				accumulate(document.createTextNode(value));
-			}
-
-			function accumulateInlineElement( tagName, $node )
-			{
-				// create a clean element
-				var $el	= $( $node.get(0).cloneNode(false) ),
-				temp;
-				
-				if ( tagName == 'span' )
-				{
-					if ( $node.css( 'fontWeight' ) == 'bold' )
-					{
-						temp = $('<strong/>').get(0);
-						accumulate( temp );
-						lineContainer = temp;
-					}
-					if ( $node.css('fontStyle') == 'italic' )
-					{
-						temp = $('<em/>').get(0);
-						accumulate( temp );
-						lineContainer = temp;
-					}
-				}
-				else
-				{
-					$el = $el.get(0);
-					accumulate( $el );
-					lineContainer = $el;
-				}
-			}
-
-			function accumulate(node)
-			{
-				if ( mode != EXPECTING_LIST_ITEM )
-				{
-					if ( ! line )
-					{
-						line = lineContainer = createLine();
-					}
-					previousAccumulation = node;
-					lineContainer.appendChild(node);
-				}
-			}
-
-			function getPreviouslyAccumulatedTagName()
-			{
-				if ( previousAccumulation &&
-					 previousAccumulation.nodeType == Node.ELEMENT_NODE )
-				{
-					return previousAccumulation.tagName.toLowerCase();
-				}
-			}
-
-			function flush()
-			{
-				if ( line && line.childNodes.length )
-				{
-					$container.append( $(line) );
-					line = lineContainer = null;
-				}
-			}
-
-			function createLine()
-			{
-				var $el;
-				if ( mode == ACCUMULATING_LINE )
-				{
-					$el = $('<div></div>');
-				}
-				else if (mode == ACCUMULATING_LIST_ITEM)
-				{
-					$el = $('<li></li>');
-				}
-				return $el.get(0);
-			}
-
-			function insertList(tagName)
-			{
-				return $('<'+tagName+'></'+tagName+'>')
-							.appendTo( $result );
-			}
+			$container;
+			//$p		= $('<p></p>'),
+			//mode	= ACCUMULATING_LINE,
+			//$result, $container,
+			//line, lineContainer,
+			//previousAccumulation;
+            //
+			//function walk( nodes )
+			//{
+			//	var
+			//	length	= nodes.length,
+			//	node, $node, $newNode, tagName, i;
+            //
+			//	for ( i=0; i < length; i++ )
+			//	{
+			//		node	= nodes[i];
+			//		$node	= $(node);
+			//		
+			//		if ( node.nodeType == Node.ELEMENT_NODE )
+			//		{
+			//			tagName = node.tagName.toLowerCase();
+			//			open( tagName, node );
+			//			walk( node.childNodes );
+			//			close( tagName );
+            //
+			//		}
+			//		else if ( node.nodeType == Node.TEXT_NODE )
+			//		{
+			//			read( node.nodeValue );
+			//		}
+			//	}
+			//}
+            //
+			//function open( tagName, node )
+			//{
+			//	$node = $(node);
+            //
+			//	if ( mode == ACCUMULATING_LINE )
+			//	{
+			//		if ( isBlockElement(tagName) )
+			//		{
+			//			if ( isEmptyParagraph( $node ) )
+			//			{
+			//				accumulate( $('<br />').get(0) );
+			//			}
+            //
+			//			flush();
+			//			
+			//			if ( isListElement(tagName) )
+			//			{
+			//				$container	= insertList( tagName );
+			//				mode		= EXPECTING_LIST_ITEM;
+			//			}
+			//		}
+			//		else if ( isLineBreak(tagName) )
+			//		{
+			//			if ( isLineBreak( getPreviouslyAccumulatedTagName() ) )
+			//			{
+			//				$(previousAccumulation).remove();
+			//				flush();
+			//			}
+            //
+			//			accumulate( $node.clone().get(0) );
+            //
+			//			if ( ! previousAccumulation.previousNode )
+			//			{
+			//				flush();
+			//			}
+			//		}
+			//		else
+			//		{
+			//			accumulateInlineElement( tagName, $node );
+			//		}
+            //
+			//	}
+			//	else if ( mode == EXPECTING_LIST_ITEM )
+			//	{
+			//		if ( isListItemElement(tagName) )
+			//		{
+			//			mode = ACCUMULATING_LIST_ITEM;
+			//		}
+            //
+			//	}
+			//	else if ( mode == ACCUMULATING_LIST_ITEM )
+			//	{
+			//		if ( isLineBreak(tagName) )
+			//		{
+			//			accumulate( $node.clone().get(0) );
+			//		}
+			//		else if ( ! isBlockElement(tagName) )
+			//		{
+			//			accumulateInlineElement( tagName, $node );
+			//		}
+			//	}
+			//}
+            //
+			//function close( tagName )
+			//{
+			//	if ( mode == ACCUMULATING_LINE )
+			//	{
+			//		if ( isLineElement( tagName ) )
+			//		{
+			//			flush();
+			//		}
+            //
+			//		if ( line != lineContainer )
+			//		{
+			//			lineContainer = lineContainer.parentNode;
+			//		}
+			//	}
+			//	else if ( mode == EXPECTING_LIST_ITEM )
+			//	{
+			//		if ( isListElement(tagName) )
+			//		{
+			//			$container = $result;
+			//			mode = ACCUMULATING_LINE;
+			//		}
+			//	}
+			//	else if ( mode == ACCUMULATING_LIST_ITEM )
+			//	{
+			//		if ( isListItemElement(tagName) )
+			//		{
+			//			flush();
+			//			mode = EXPECTING_LIST_ITEM;
+			//		}
+            //
+			//		if ( line != lineContainer )
+			//		{
+			//			lineContainer = lineContainer.parentNode;
+			//		}
+			//	}
+			//}
+            //
+			//function isBlockElement( tagName )
+			//{
+			//	return isLineElement(tagName) || isListElement(tagName);
+			//}
+            //
+			//function isLineElement( tagName )
+			//{
+			//	return	tagName == "p" ||
+			//			tagName == "div" ||
+			//			tagName == "blockquote";
+			//}
+            //
+			//function isListElement( tagName )
+			//{
+			//	return tagName == "ol" || tagName == "ul";
+			//}
+            //
+			//function isListItemElement(tagName)
+			//{
+			//	return tagName == "li";
+			//}
+            //
+			//function isLineBreak(tagName)
+			//{
+			//	return tagName == "br";
+			//}
+            //
+			//function isEmptyParagraph( $node )
+			//{
+			//	return	$node.is('p:empty') ||
+			//			( $node.is('p') && $.trim( $node.html() ) == '' );
+			//}
+            //
+			//function read(value)
+			//{
+			//	accumulate(document.createTextNode(value));
+			//}
+            //
+			//function accumulateInlineElement( tagName, $node )
+			//{
+			//	// create a clean element
+			//	var $el	= $( $node.get(0).cloneNode(false) ),
+			//	temp;
+			//	
+			//	if ( tagName == 'span' )
+			//	{
+			//		if ( $node.css( 'fontWeight' ) == 'bold' )
+			//		{
+			//			temp = $('<strong/>').get(0);
+			//			accumulate( temp );
+			//			lineContainer = temp;
+			//		}
+			//		if ( $node.css('fontStyle') == 'italic' )
+			//		{
+			//			temp = $('<em/>').get(0);
+			//			accumulate( temp );
+			//			lineContainer = temp;
+			//		}
+			//	}
+			//	else
+			//	{
+			//		$el = $el.get(0);
+			//		accumulate( $el );
+			//		lineContainer = $el;
+			//	}
+			//}
+            //
+			//function accumulate(node)
+			//{
+			//	if ( mode != EXPECTING_LIST_ITEM )
+			//	{
+			//		if ( ! line )
+			//		{
+			//			line = lineContainer = createLine();
+			//		}
+			//		previousAccumulation = node;
+			//		lineContainer.appendChild(node);
+			//	}
+			//}
+            //
+			//function getPreviouslyAccumulatedTagName()
+			//{
+			//	if ( previousAccumulation &&
+			//		 previousAccumulation.nodeType == Node.ELEMENT_NODE )
+			//	{
+			//		return previousAccumulation.tagName.toLowerCase();
+			//	}
+			//}
+            //
+			//function flush()
+			//{
+			//	if ( line && line.childNodes.length )
+			//	{
+			//		$container.append( $(line) );
+			//		line = lineContainer = null;
+			//	}
+			//}
+            //
+			//function createLine()
+			//{
+			//	var $el;
+			//	if ( mode == ACCUMULATING_LINE )
+			//	{
+			//		$el = $('<div></div>');
+			//	}
+			//	else if (mode == ACCUMULATING_LIST_ITEM)
+			//	{
+			//		$el = $('<li></li>');
+			//	}
+			//	return $el.get(0);
+			//}
+            //
+			//function insertList(tagName)
+			//{
+			//	return $('<'+tagName+'></'+tagName+'>')
+			//				.appendTo( $result );
+			//}
 
 			function cleanup()
 			{
@@ -325,6 +327,7 @@ WysiHat.Formatting = (function($){
 								.replace('<br></div>','</p>')
 								.replace('<div>','<p>')
 								.replace('</div>','</p>')
+								.replace('<p>&nbsp;</p>','')
 								// Fancy formatting
 									.replace(/<\/(a|p|hr|pre|ul|ol|dl|div|h[1-6]|hgroup|address|blockquote|ins|del|object|map|noscript|section|nav|article|aside|header|footer|video|audio|figure|table|thead|tfoot|tbody|tr|th|td|form|fieldset|menu|canvas|details|embed)>/,'</$1>\n')
 								.replace(/\n+/,'\n')
@@ -362,9 +365,9 @@ WysiHat.Formatting = (function($){
 				$el.replaceWith( $new );
 			}
 			
-			$result = $container = $('<div></div>');
-			walk( $clone.get(0).childNodes );
-			flush();
+			$container = $('<div></div>').html($clone.html());
+			//walk( $clone.get(0).childNodes );
+			//flush();
 			cleanup();
 			return $container.html();
 		}
