3b72421e2988446496152e11335df4a97c3ae24e
aarongustafson <aaron@easy-designs.net>
2011-09-21 17:20:56 -0400

    extend once instead of twice (to reduce code)

--- src/wysihat/dom/bookmark.js
+++ src/wysihat/dom/bookmark.js
@@ -5,77 +5,84 @@
 	if ( $.browser.msie )
 	{
 
-		Selection.prototype.setBookmark = function()
+		function setBookmark()
 		{
 			var
 			$bookmark	= $('#wysihat-bookmark'),
 			$parent		= $('<div/>'),
 			range		= this._document.selection.createRange();
-			
+
 			if ( $bookmark.length > 0 )
 			{
 				$bookmark.remove();
 			}
-			
+
 			$bookmark = $( '<span id="wysihat-bookmark">&nbsp;</span>' )
 							.appendTo( $parent );
-			
+
 			range.collapse();
 			range.pasteHTML( $parent.html() );
-	    };
-		Selection.prototype.moveToBookmark = function()
+		}
+
+		function moveToBookmark(element)
 		{
 			var
 			$bookmark	= $('#wysihat-bookmark'),
 			range		= this._document.selection.createRange();
-			
+
 			if ( $bookmark.length > 0 )
 			{
 				$bookmark.remove();
 			}
-			
+
 			range.moveToElementText( $bookmark.get(0) );
 			range.collapse();
 			range.select();
-			
+
 			$bookmark.remove();
-	    };
+		}
 	
 	}
 	else
 	{
 
-		Selection.prototype.setBookmark = function()
+		function setBookmark()
 		{
 			var $bookmark	= $('#wysihat-bookmark');
-			
+
 			if ( $bookmark.length > 0 )
 			{
 				$bookmark.remove();
 			}
-			
+
 			$bookmark = $( '<span id="wysihat-bookmark">&nbsp;</span>' );
-			
+
 			this.getRangeAt(0).insertNode( $bookmark.get(0) );
-	    };
-		Selection.prototype.moveToBookmark = function()
+		}
+
+		function moveToBookmark(element)
 		{
 			var
 			$bookmark	= $('#wysihat-bookmark'),
 			range		= DOC.createRange();
-			
+
 			if ( $bookmark.length > 0 )
 			{
 				$bookmark.remove();
 			}
-			
+
 			range.setStartBefore( $bookmark.get(0) );
 			this.removeAllRanges();
 			this.addRange(range);
-			
-			$bookmark.remove();
-	    };
 
+			$bookmark.remove();
+		}
+			
 	}
+	
+	$.extend( Selection.prototype, {
+		setBookmark:    setBookmark,
+		moveToBookmark: moveToBookmark
+	});
 
 })(document,jQuery);
--- src/wysihat/dom/selection.js
+++ src/wysihat/dom/selection.js
@@ -8,58 +8,46 @@ if ( typeof Selection == 'undefined' )
 	Selection.prototype = window.getSelection().__proto__;
 }
 
-(function($){
+(function( DOC, $ ){
 	
-	if ($.browser.msie) {
+	if ( $.browser.msie )
+	{
 	
-		$.extend(Selection.prototype, (function(){
-			
-			// TODO: More robust getNode
-			function getNode() {
-				var range = this._document.selection.createRange();
-				return $(range.parentElement());
-			}
+		// TODO: More robust getNode
+		function getNode() {
+			var range = this._document.selection.createRange();
+			return $(range.parentElement());
+		}
 
-			// TODO: IE selectNode should work with range.selectNode
-			function selectNode(element) {
-				var range = this._document.body.createTextRange();
-				range.moveToElementText(element);
-				range.select();
-			}
-
-			return {
-				getNode:    getNode,
-				selectNode: selectNode
-			};
-			
-		})());
+		// TODO: IE selectNode should work with range.selectNode
+		function selectNode(element) {
+			var range = this._document.body.createTextRange();
+			range.moveToElementText(element);
+			range.select();
+		}
 
 	}
 	else
 	{
 
-		$.extend(Selection.prototype, (function(){
+		function getNode()
+		{
+			return ( this.rangeCount > 0 ) ? this.getRangeAt(0).getNode() : null;
+		}
 
-			function getNode()
-			{
-				return ( this.rangeCount > 0 ) ? this.getRangeAt(0).getNode() : null;
-			}
+		function selectNode(element)
+		{
+			var range = document.createRange();
+			range.selectNode(element[0]);
+			this.removeAllRanges();
+			this.addRange(range);
+		}
 
-			function selectNode(element)
-			{
-				var range = document.createRange();
-				range.selectNode(element[0]);
-				this.removeAllRanges();
-				this.addRange(range);
-			}
-
-			return {
-				getNode:    getNode,
-				selectNode: selectNode
-			};
-
-		})());
 	}
 	
+	$.extend(Selection.prototype, {
+		getNode:    getNode,
+		selectNode: selectNode
+	});
 	
-})(jQuery);
+})( document, jQuery );
